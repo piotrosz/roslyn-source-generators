05ea5373b75471525c5e20ece0b4d595ee2705e7
Piotr Ludwiczuk <pilu@foss.pl>
2023-06-16 16:29:10 +0200

    Add first example of source generator

---
+++ ConsoleAppSourceGen/ConsoleAppSourceGen/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleAppSourceGen
+{
+    public static partial class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloFrom("Generated Code");
+        }
+
+        static partial void HelloFrom(string name);
+    }
+}
